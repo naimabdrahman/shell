40f30de04c4e75a99094d04de738a8c8d63ab216
naimabdrahman <naim.abdrahman@gmail.com>
2017-09-06 12:19:28 +0800

    Update conf-mongo-epel.sh

--- conf-mongo-epel.sh
+++ conf-mongo-epel.sh
@@ -2,8 +2,6 @@
 #MongoDB shell version: 2.6.12
 #to configure /etc/mongod.conf
 
-
-
 # create backup and tempfile
 
 if test -f /etc/mongod.conf.bckup ; then echo -e "/etc/mongod.conf.bckup exist"; else cat /etc/mongod.conf > /etc/mongod.conf.bckup; fi
@@ -13,13 +11,19 @@ if test -f temp2.txt ; then echo -e "temp2.txt exist"; else cat /etc/mongod.conf
 
 
 #IP
-nmcli  | grep inet4 | awk  '{print $2}' | awk -F\/ '{print $1}'
+IP=`nmcli| grep inet4 | awk  '{print $2}' | awk -F\/ '{print $1}'`
+#echo $IP
 
 
-
-
-
-#editing configuration of /etc/mongod.conf 
+#editing configuration of /etc/mongod.conf
 #replace bind_ip
-cat temp2.txt  | sed -e 's/#port = 27017/port = 27017/g' | sed -e 's/#keyFile = arg/keyFile = \/opt\/mongo\/mongo-keyfile/g' | sed -e 's/#replSet = arg/replSet = rs0/g' | sed -e 's/bind_ip = 127.0.0.1/bind_ip = 127.0.0.1,$IP/g'> temp3.txt
+cat temp2.txt | sed -e 's/#port = 27017/port = 27017/g' | sed -e 's/#keyFile = arg/keyFile = \/opt\/mongo\/mongo-keyfile/g' | sed -e 's/#replSet = arg/replSet = rs0/g' | sed -e 's/bind_ip = 127.0.0.1/bind_ip = 127.0.0.1,'$IP'/g'> temp3.txt
 cat temp3.txt > /etc/mongod.conf
+
+
+
+
+#editing configuration of /etc/mongod.conf
+#dont forget to replace bind_ip
+#cat temp2.txt  | sed -e 's/#port = 27017/port = 27017/g' | sed -e 's/#keyFile = arg/keyFile = \/opt\/mongo\/mongo-keyfile/g' | sed -e 's/#replSet = arg/replSet = rs0/g' | sed -e 's/bind_ip = 127.0.0.1/bind_ip = 127.0.0.1,10.124.20.57/g'> temp3.txt
+#cat temp3.txt > /etc/mongod.conf
