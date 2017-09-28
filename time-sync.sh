338a6649e4e6b2902644121457b1421fd9a2af51
naimabdrahman <naim.abdrahman@gmail.com>
2017-09-28 17:15:38 +0800

    Create time-sync.sh

---
+++ time-sync.sh
@@ -0,0 +1,12 @@
+#to sync time between linux servers without NTP
+#Use either method1 or method2
+#seamless ssh is required
+
+#method1
+#date > tmp
+#cat tmp | ssh localhost 'cat> tmp2 && date -s "`cat tmp2`"'
+
+#or
+
+#method2
+date | ssh localhost 'cat> tmp2 && date -s "`cat tmp2`"'
