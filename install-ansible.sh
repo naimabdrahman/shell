004b0f9f7fd47bc9cc8305a3b523851485092b38
naimabdrahman <naim.abdrahman@gmail.com>
2017-11-19 21:29:49 +0800

    Create install-ansible.sh

---
+++ install-ansible.sh
@@ -0,0 +1,12 @@
+#install ansible on centos 7
+yum --enablerepo=epel -y install ansible openssh-clients 
+cat /etc/ansible/ansible.cfg  > /etc/ansible/ansible.cfg.bckup
+cat /etc/ansible/ansible.cfg.bckup | sed 's/#host_key_checking = False/host_key_checking = False/g' > /etc/ansible/ansible.cfg
+
+
+
+cat /etc/ansible/hosts > /etc/ansible/hosts.bckup
+echo -e "[local]" > /etc/ansible/hosts
+echo -e "localhost ansible_connection=ssh ansible_ssh_user=root ansible_ssh_pass=<INSERT>" >> /etc/ansible/hosts
+echo -e "\n\npls add passord in /etc/ansible/hosts before using it\n\n "
+
