976791c893a1d346365729cc87fd3333e4201dd9
naimabdrahman <naim.abdrahman@gmail.com>
2017-11-22 11:27:54 +0800

    Update install-go.sh

--- install-go.sh
+++ install-go.sh
@@ -2,4 +2,4 @@
 # repo is located in https://go-repo.io/
 rpm --import https://mirror.go-repo.io/centos/RPM-GPG-KEY-GO-REPO
 curl -s https://mirror.go-repo.io/centos/go-repo.repo | tee /etc/yum.repos.d/go-repo.repo
-yum install golang
+yum -y install golang
