f7ea6afe8c0fc010f46bbcf46b4402b9d7908e61
naimabdrahman <naim.abdrahman@gmail.com>
2017-11-22 11:24:28 +0800

    Create install-go.sh

---
+++ install-go.sh
@@ -0,0 +1,5 @@
+# install go on centos 7
+# repo is located in https://go-repo.io/
+rpm --import https://mirror.go-repo.io/centos/RPM-GPG-KEY-GO-REPO
+curl -s https://mirror.go-repo.io/centos/go-repo.repo | tee /etc/yum.repos.d/go-repo.repo
+yum install golang
