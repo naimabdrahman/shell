c5ba44287502efe6b987645c663eeef9f84ff049
naimabdrahman <naim.abdrahman@gmail.com>
2017-12-07 15:55:56 +0800

    Create install-golang-custom.sh

---
+++ install-golang-custom.sh
@@ -0,0 +1,23 @@
+# install go (go1.9.2.linux-amd64.tar.gz) on centos 7
+# package go1.9.2.linux-amd64.tar.gz can be updated from time to time
+
+yum -y install wget
+
+mkdir /root/golang
+cd /root/golang
+wget https://redirector.gvt1.com/edgedl/go/go1.9.2.linux-amd64.tar.gz
+sleep 1
+tar -zxvf *.tar.gz
+sleep 1
+
+echo -e '\n' >>  >>/root/.bashrc
+echo -e '### GO ###' >>/root/.bashrc
+echo -e 'export GOROOT=$HOME/golang/go' >>/root/.bashrc
+echo -e 'export PATH=$PATH:$GOROOT/bin' >>/root/.bashrc
+
+sleep 1
+source /root/.bashrc
+
+sleep 1 
+go version
+go env
