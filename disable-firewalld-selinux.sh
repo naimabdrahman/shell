e2d516561f4b587bdf202b2bda2c3840b99b3dab
naimabdrahman <naim.abdrahman@gmail.com>
2017-12-26 12:10:13 +0800

    Create disable-firewalld-selinux.sh

---
+++ disable-firewalld-selinux.sh
@@ -0,0 +1,6 @@
+# Disable firewall and SELinux on centos 7
+systemctl disable firewalld
+systemctl stop firewalld
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+setenforce 0
+reboot
