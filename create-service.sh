04276e5e8dbf5f6deac546a79915042ce3aded0f
naimabdrahman <naim.abdrahman@gmail.com>
2018-05-15 16:08:02 +0800

    Update create-service.sh

--- create-service.sh
+++ create-service.sh
@@ -11,7 +11,7 @@ echo -e "User=root">> /etc/systemd/system/lalala.service
 echo -e "ExecStart=/root/jmeter/bin/lalala-server" >> /etc/systemd/system/lalala.service
 echo -e "" >> /etc/systemd/system/lalala.service
 echo -e "[Install]" >> /etc/systemd/system/lalala.service
-echo -e "WantedBy=default.target" >> /etc/systemd/system/lalala.service
+echo -e "WantedBy=multi-user.target" >> /etc/systemd/system/lalala.service
 
 systemctl start lalala.service
 systemctl enable lalala.service
