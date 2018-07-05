821d872c998c476d56f4fc0baf37f42858807bca
naimabdrahman <naim.abdrahman@gmail.com>
2018-07-05 16:33:20 +0800

    Update install-fluentd.sh

--- fluentd/install-fluentd.sh
+++ fluentd/install-fluentd.sh
@@ -1,4 +1,4 @@
-# install fluentd service on centos 7
+## install fluentd service on centos 7
 curl -L https://toolbelt.treasuredata.com/sh/install-redhat-td-agent3.sh | sh
 systemctl start td-agent.service
 chkconfig td-agent on
@@ -8,8 +8,13 @@ sleep 3
 
 
 
-# test to log data 
+## test to log data 
 curl -X POST -d 'json={"json":"message"}' http://localhost:8888/debug.test
 
-# validate data logged
+## validate data logged
 cat /var/log/td-agent/td-agent.log
+
+
+## install plugins for bigquery
+#td-agent-gem install fluent-plugin-bigquery -v 2.0.0.beta
+
