9645f97e4cf87c4060de53854b9352daca63f72b
naimabdrahman <naim.abdrahman@gmail.com>
2018-07-13 09:45:09 +0800

    Update install-kimchi.sh

--- install-kimchi.sh
+++ install-kimchi.sh
@@ -1,9 +1,10 @@
 # install kimchi on centos 7
 
+yum -y install wget
 mkdir -p /root/tmp ; cd /root/tmp
-wget http://kimchi-project.github.io/wok/downloads/latest/wok.el7.centos.noarch.rpm
+wget https://github.com/kimchi-project/wok/releases/download/2.5.0/wok-2.5.0-0.el7.centos.noarch.rpm
 wget http://kimchi-project.github.io/gingerbase/downloads/latest/ginger-base.el7.centos.noarch.rpm
-wget http://kimchi-project.github.io/kimchi/downloads/latest/kimchi.el7.centos.noarch.rpm
+wget https://github.com/kimchi-project/kimchi/releases/download/2.5.0/kimchi-2.5.0-0.el7.centos.noarch.rpm
 yum -y install wok.el7.centos.noarch.rpm
 yum -y install ginger-base.el7.centos.noarch.rpm
 yum -y install kimchi.el7.centos.noarch.rpm
