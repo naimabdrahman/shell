257f08b18fef98f61f60621d2e2644e231b50b47
naimabdrahman <naim.abdrahman@gmail.com>
2018-07-13 09:57:35 +0800

    Update install-kimchi.sh

--- install-kimchi.sh
+++ install-kimchi.sh
@@ -5,7 +5,7 @@ mkdir -p /root/tmp ; cd /root/tmp
 wget https://github.com/kimchi-project/wok/releases/download/2.5.0/wok-2.5.0-0.el7.centos.noarch.rpm
 wget http://kimchi-project.github.io/gingerbase/downloads/latest/ginger-base.el7.centos.noarch.rpm
 wget https://github.com/kimchi-project/kimchi/releases/download/2.5.0/kimchi-2.5.0-0.el7.centos.noarch.rpm
-yum -y install *.rpm
+yum -y install *.rpm  --skip-broken
 sed -i  's/^#session_timeout = .*/session_timeout = 1440/g' /etc/wok/wok.conf
 systemctl enable wokd
 systemctl start wokd
