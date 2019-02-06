d4fd42075bcbf37d7a1505e675cdfc12976c3ea8
naimabdrahman <naim.abdrahman@gmail.com>
2019-02-06 14:33:56 +0800

    Wed Feb  6 14:33:56 +08 2019

---
+++ vncserver-kill.sh
@@ -0,0 +1,20 @@
+## script to kill my vnc session
+## tested on centos7
+
+# check whoami
+xxx=`whoami`
+echo $xxx ### to check variable xxx
+
+# check my session number
+lalala=`ps -ef | grep vnc | grep $xxx | grep  Xvnc | awk  '{print $9}' | grep -v auto`
+echo $lalala ### to check variable lalala
+
+
+# kill session
+vncserver -kill $lalala
+
+
+# job completed or not verification
+if test $? -eq 0 ; then echo -e "\njob completed"; else echo -e "\njob not completed"; fi
+
+ 
