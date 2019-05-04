1e77725317c3382c0a9155b8a80aa77913552942
naimabdrahman <naim.abdrahman@gmail.com>
2019-05-04 18:57:04 +0800

    Update conf-eth0.sh

--- conf-eth0.sh
+++ conf-eth0.sh
@@ -1,4 +1,6 @@
-# configure eth0 to manual IP
+## configure eth0 to manual IP
+## tested with enp0s3 centos 7
+
 nmcli c modify eth0 ipv4.addresses 192.168.x.x/24
 nmcli c modify eth0 ipv4.gateway 192.168.x.x
 nmcli c modify eth0 ipv4.dns 8.8.8.8
