46e4f9040d34f730b76895e67f666f6982e867f9
naimabdrahman <naim.abdrahman@gmail.com>
2019-05-09 18:35:16 +0800

    Update rest-api.sh

--- rest-api.sh
+++ rest-api.sh
@@ -3,3 +3,4 @@ curl -i -X GET http://rest-api.io/items/5069b47aa892630aae059584
 curl -i -X DELETE http://rest-api.io/items/5069b47aa892630aae059584
 curl -i -X POST -H 'Content-Type: application/json' -d '{"name": "New item", "year": "2009"}' http://rest-api.io/items
 curl -i -X PUT -H 'Content-Type: application/json' -d '{"name": "Updated item", "year": "2010"}' http://rest-api.io/items/5069b47aa892630aae059584
+curl -u username:password http://example.com
