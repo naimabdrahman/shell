887cd942a106a75085ed37664e181e5275a9b2f1
naimabdrahman <naim.abdrahman@gmail.com>
2019-06-22 15:59:22 +0800

    Create conf-enp0s3-mobile.sh

---
+++ conf-enp0s3-mobile.sh
@@ -0,0 +1,8 @@
+## configure enp0s3 to manual IP
+## tested with enp0s3 centos 7
+
+nmcli c modify eth0 ipv4.addresses 192.168.43.41/24
+nmcli c modify eth0 ipv4.gateway 192.168.43.1
+nmcli c modify eth0 ipv4.dns 8.8.8.8
+nmcli c modify eth0 ipv4.method manual
+reboot
