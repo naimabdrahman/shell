e107fc915dad72835a0ba6cc39fd938a4729907b
naimabdrahman <naim.abdrahman@gmail.com>
2019-06-28 17:12:18 +0800

    Update install-apache-exporter.sh

--- install-apache-exporter.sh
+++ install-apache-exporter.sh
@@ -1,6 +1,14 @@
-# install apache exporter on centos 7
-# make sure golang is installed
-# step modified from https://machineperson.github.io/monitoring/2016/01/04/exporting-apache-metrics-to-prometheus.html
+## install apache exporter on centos 7
+## make sure golang is installed
+## step modified from https://machineperson.github.io/monitoring/2016/01/04/exporting-apache-metrics-to-prometheus.html
+
+
+# install golang on centos 7
+# repo is located in https://go-repo.io/
+rpm --import https://mirror.go-repo.io/centos/RPM-GPG-KEY-GO-REPO
+curl -s https://mirror.go-repo.io/centos/go-repo.repo | tee /etc/yum.repos.d/go-repo.repo
+yum -y install golang
+
 
 mkdir ~/go
 export GOPATH=$HOME/go
