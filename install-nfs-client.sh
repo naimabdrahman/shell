e55cb9f071a002192ab481dd2d0422b7b994ca27
naimabdrahman <naim.abdrahman@gmail.com>
2019-06-29 21:23:06 +0800

    Update install-nfs-client.sh

--- install-nfs-client.sh
+++ install-nfs-client.sh
@@ -1,15 +1,16 @@
-#install nfs client on centos 7
+## install nfs client on centos 7
+## nfs server is node3.nar.test
 
 yum -y install nfs-utils
-cat /etc/idmapd.conf > /etc/idmapd.conf.bckup
-cat /etc/idmapd.conf.bckup | sed 's/#Domain = local.domain.edu/nar.test/g' > /etc/idmapd.conf
+#cat /etc/idmapd.conf > /etc/idmapd.conf.bckup
+#cat /etc/idmapd.conf.bckup | sed 's/#Domain = local.domain.edu/nar.test/g' > /etc/idmapd.conf
 
 systemctl start rpcbind
 systemctl enable rpcbind
 
 mkdir /mnt/temp
-mount -t nfs node1.nar.test:/data /mnt/temp  ##host can be replaced with IP
+mount -t nfs node3.nar.test:/data /mnt/temp  ##host can be replaced with IP # currently node3
 
 df -hT
 
-echo "node1.nar.test:/data  /mnt/temp                   nfs     defaults        0 0 " >>/etc/fstab
+echo "node3.nar.test:/data  /mnt/temp                   nfs     defaults        0 0 " >>/etc/fstab
