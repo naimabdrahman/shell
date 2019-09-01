2f695ddc82b7a6a062a5c24d17c407e8b901ba57
naimabdrahman <naim.abdrahman@gmail.com>
2019-09-01 22:10:27 +0800

    Update create-ssl.sh

--- create-ssl.sh
+++ create-ssl.sh
@@ -1,5 +1,13 @@
+## create ssl 
+
+## option 1 
 cd /etc/pki/tls/certs
 make server.key
 openssl rsa -in server.key -out server.key
 make server.csr
 openssl x509 -in server.csr -out server.cert -req -signkey server.key -days 3650
+
+
+## option 2
+#cd /etc/pki/tls/certs
+#openssl req -x509 -nodes -newkey rsa:2048 -keyout /etc/pki/tls/certs/haproxy.pem -out /etc/pki/tls/certs/haproxy.pem -days 3650 
