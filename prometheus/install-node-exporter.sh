7f071eb4ff50c50f5500a29ae3e714464fbb3190
naimabdrahman <naim.abdrahman@gmail.com>
2019-09-05 17:21:55 +0800

    Update install-node-exporter.sh

--- prometheus/install-node-exporter.sh
+++ prometheus/install-node-exporter.sh
@@ -24,7 +24,7 @@ echo -e "">> /etc/systemd/system/node_exporter.service
 echo -e "[Service]" >> /etc/systemd/system/node_exporter.service
 echo -e "User=root">> /etc/systemd/system/node_exporter.service
 #echo -e "ExecStart=/root/node_exporter/node_exporter" >> /etc/systemd/system/node_exporter.service #old
-echo -e "ExecStart=node_exporter/node_exporter-0.18.1.linux-amd64/node_exporter" >> /etc/systemd/system/node_exporter.service
+echo -e "ExecStart=/root/node_exporter/node_exporter-0.18.1.linux-amd64/node_exporter" >> /etc/systemd/system/node_exporter.service
 echo -e "" >> /etc/systemd/system/node_exporter.service
 echo -e "[Install]" >> /etc/systemd/system/node_exporter.service
 echo -e "WantedBy=default.target" >> /etc/systemd/system/node_exporter.service
