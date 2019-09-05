f4e210264603a0f8e487077f586f8fb5acde4de9
naimabdrahman <naim.abdrahman@gmail.com>
2019-09-05 13:22:25 +0800

    Create extract-time.sh

---
+++ textprocessing/extract-time.sh
@@ -0,0 +1,14 @@
+## get printable date from date/timestamp
+## usefulfor creating backup file based on timestamp
+
+
+
+## option 1
+#date +"%Y%m%d"
+
+## option 2
+date +"%Y-%m-%d"
+
+
+## option 3
+#date +"%Y%m%d_%H%M%S"
