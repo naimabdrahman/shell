81c09e3997d9f17050573fdee68673b86948a330
naimabdrahman <naim.abdrahman@gmail.com>
2019-09-05 08:40:57 +0000

    Thu Sep  5 08:40:19 UTC 2019

--- create-service.sh
+++ create-service.sh
@@ -1,17 +1,27 @@
 #create service in centos 7
 
-touch /etc/systemd/system/multi-user.target.wants/lalala.service
+vars=prometheus
 
-echo -e "[Unit]" > /etc/systemd/system/lalala.service
-echo -e "Description=lalala server" >> /etc/systemd/system/lalala.service
-echo -e "After = network.target " >> /etc/systemd/system/lalala.service
-echo -e "">> /etc/systemd/system/lalala.service
-echo -e "[Service]" >> /etc/systemd/system/lalala.service
-echo -e "User=root">> /etc/systemd/system/lalala.service
-echo -e "ExecStart=/root/jmeter/bin/lalala-server" >> /etc/systemd/system/lalala.service
-echo -e "" >> /etc/systemd/system/lalala.service
-echo -e "[Install]" >> /etc/systemd/system/lalala.service
-echo -e "WantedBy=multi-user.target" >> /etc/systemd/system/lalala.service
+touch /etc/systemd/system/multi-user.target.wants/$vars.service
 
-systemctl start lalala.service
-systemctl enable lalala.service
+echo -e "[Unit]" > /etc/systemd/system/$vars.service
+echo -e "Description=$vars server" >> /etc/systemd/system/$vars.service
+echo -e "After = network.target " >> /etc/systemd/system/$vars.service
+echo -e "">> /etc/systemd/system/$vars.service
+echo -e "[Service]" >> /etc/systemd/system/$vars.service
+echo -e "User=root">> /etc/systemd/system/$vars.service
+echo -e "ExecStart=/root/jmeter/bin/$vars-server" >> /etc/systemd/system/$vars.service  # service 
+echo -e "" >> /etc/systemd/system/$vars.service
+echo -e "[Install]" >> /etc/systemd/system/$vars.service
+echo -e "WantedBy=multi-user.target" >> /etc/systemd/system/$vars.service
+
+
+## tweak path
+mv /etc/systemd/system/multi-user.target.wants/$vars.service /usr/lib/systemd/system/
+
+
+
+## enable service
+systemctl daemon-reload
+systemctl start $vars.service
+systemctl enable $vars.service
