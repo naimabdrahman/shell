87410dc30356a8c8d48d48a408d8b2eae3419f1a
naimabdrahman <naim.abdrahman@gmail.com>
2019-09-10 15:01:56 +0800

    Update firewalld.sh

--- firewalld.sh
+++ firewalld.sh
@@ -5,3 +5,9 @@ firewall-cmd --permanent --add-port=22/tcp
 firewall-cmd --permanent --add-port=27017/tcp
 firewall-cmd --reload
 chkconfig firewalld on
+
+
+
+
+## Get available service
+#firewall-cmd --get-services 
