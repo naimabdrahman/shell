1f0b52824b14f05ed72c06269045c8293b40a27f
naimabdrahman <naim.abdrahman@gmail.com>
2019-09-28 04:58:03 +0000

    Sat Sep 28 04:57:15 UTC 2019

--- prometheus/install-prometheus.sh
+++ prometheus/install-prometheus.sh
@@ -2,21 +2,42 @@
 
 package=prometheus-2.12.0.linux-amd64
 
+
+## preparation
 yum -y install wget
 sleep 2
 mkdir  /root/dl
 cd /root/dl
 
-sleep 2
-#wget https://github.com/prometheus/prometheus/releases/download/v2.0.0/prometheus-2.0.0.linux-amd64.tar.gz
+
+## download and unpack package
 wget https://github.com/prometheus/prometheus/releases/download/v2.12.0/$package.tar.gz
 sleep 2
 tar -zxvf $package.tar.gz
 mv $package /root/
 cd /root/$package/
 
-sleep 2
-./prometheus --config.file=prometheus.yml &
 
-sleep 2
+## quick run prometheus to see if it is working ## not in used. service is created instead
+#/root/prometheus-2.12.0.linux-amd64/prometheus --config.file=/root/prometheus-2.12.0.linux-amd64/prometheus.yml &
+
+## create prometheus service
+touch /etc/systemd/system/prometheus.service
+echo -e "[Unit]" > /etc/systemd/system/prometheus.service
+echo -e "Description=Prometheus" >> /etc/systemd/system/prometheus.service
+echo -e "">> /etc/systemd/system/prometheus.service
+echo -e "[Service]" >> /etc/systemd/system/prometheus.service
+echo -e "User=root">> /etc/systemd/system/prometheus.service
+echo -e "ExecStart=/root/prometheus-2.12.0.linux-amd64/prometheus --config.file=/root/prometheus-2.12.0.linux-amd64/prometheus.yml" >> /etc/systemd/system/prometheus.service
+echo -e "" >> /etc/systemd/system/prometheus.service
+echo -e "[Install]" >> /etc/systemd/system/prometheus.service
+echo -e "WantedBy=default.target" >> /etc/systemd/system/prometheus.service
+
+
+## enable service and run permanently
+systemctl daemon-reload
+systemctl enable prometheus.service
+systemctl start prometheus.service
+
+## verification
 echo -e "\n\npls browse to http://IP:9090 to verify that prometheus working properly\n\n"
