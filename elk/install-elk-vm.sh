b6161bcfb241700255effa3e15b06d4278a9995a
naimabdrahman <naim.abdrahman@gmail.com>
2019-10-01 09:47:11 +0800

    Update install-elk-vm.sh

--- elk/install-elk-vm.sh
+++ elk/install-elk-vm.sh
@@ -35,13 +35,13 @@ sleep 5
 
 ## Tweaking kibana config and restart kibana service
 ## take backup
-#cat /etc/kibana/kibana.yml > /etc/kibana/kibana.yml.bckup.`date +%s`
+cat /etc/kibana/kibana.yml > /root/kibana.yml.bckup.`date +%s`
 ## update config via sed and restart 
-#sed -i 's/#server.port: 5601/server.port: 5601/' /etc/kibana/kibana.yml
-#sed -i 's/#server.host: "localhost"/server.host: "0.0.0.0"/g' /etc/kibana/kibana.yml
-#sed -i 's/#elasticsearch.hosts:/elasticsearch.hosts/g' /etc/kibana/kibana.yml
-#sleep 5
-#systemctl restart kibana.service
+sed -i 's/#server.port: 5601/server.port: 5601/' /etc/kibana/kibana.yml
+sed -i 's/#server.host: "localhost"/server.host: "0.0.0.0"/g' /etc/kibana/kibana.yml
+sed -i 's/#elasticsearch.hosts: ["http://localhost:9200"]/elasticsearch.hosts: ["http://localhost:9200"]/g' /etc/kibana/kibana.yml
+sleep 5
+systemctl restart kibana.service
 
 
 
