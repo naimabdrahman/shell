1df0eb1a022cb8f16096f59a31ca91bedf2fddce
naimabdrahman <naim.abdrahman@gmail.com>
2019-10-02 15:23:00 +0800

    Create processing-URL.sh

---
+++ textprocessing/processing-URL.sh
@@ -0,0 +1,35 @@
+## Processing and cleaning bad URL list
+## list.txt shouldve been added into the 'list' dir
+
+
+
+## setting up path
+MYPATH=`pwd`
+STAGING=$MYPATH/staging
+BCKUP=$MYPATH/bckup
+LIST=$MYPATH/list
+
+
+
+## preparation
+
+mkdir -p $STAGING
+mkdir -p $BCKUP
+mkdir -p $LIST
+
+
+## process list
+for x in $LIST/*.txt ; do cat $x  | sed -e 's/\[//g' | sed -e 's/\]//g' | sed -e 's/hxxp/http/g' > $STAGING/list-stg.txt ; done
+
+
+## cleaning list - remove dedup
+sort $STAGING/list-stg.txt | uniq -u > $STAGING/list-stg.txt.temp
+mv $STAGING/list-stg.txt.temp $STAGING/list-stg.txt
+
+
+## completed
+echo -e "
+To test pls check the file :  $STAGING/list-stg.txt
+"
+
+
