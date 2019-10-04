40c9d08196ae1b73776c5cfa3afddf1b401ec175
naimabdrahman <naim.abdrahman@gmail.com>
2019-10-04 12:43:41 +0800

    Update processing-URL.sh

--- textprocessing/processing-URL.sh
+++ textprocessing/processing-URL.sh
@@ -19,7 +19,7 @@ mkdir -p $LIST
 
 
 ## process list
-for x in $LIST/*.txt ; do cat $x  | sed -e 's/\[//g' | sed -e 's/\]//g' | sed -e 's/hxxp/http/g' > $STAGING/list-stg.txt ; done
+for x in $LIST/*.txt ; do cat $x | sed -e 's/\[dot\]/./g'  | sed -e 's/\(dot\)/./g' | sed -e 's/\[//g' | sed -e 's/\]//g' | sed -e 's/hxxp/http/g' > $STAGING/list-stg.txt ; done
 
 
 ## cleaning list - remove dedup
@@ -31,5 +31,4 @@ mv $STAGING/list-stg.txt.temp $STAGING/list-stg.txt
 echo -e "
 To test pls check the file :  $STAGING/list-stg.txt
 "
-
 
