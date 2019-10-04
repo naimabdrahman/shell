17b6eb2d1c339108150ff5190a30569bda3f01f2
naimabdrahman <naim.abdrahman@gmail.com>
2019-10-04 08:42:25 +0000

    lalala

--- textprocessing/processing-URL.sh
+++ textprocessing/processing-URL.sh
@@ -1,13 +1,18 @@
 ## Processing and cleaning bad URL list
 ## list.txt shouldve been added into the 'list' dir
 
-## setting up path
+## setting up path and variables
 MYPATH=`pwd`
 STAGING=$MYPATH/staging
 BCKUP=$MYPATH/bckup
 LIST=$MYPATH/list
 SOURCEPATH=https://raw.githubusercontent.com/naimabdrahman/shell/master/textprocessing/ ## <update accordingly>
 SOURCELIST=listofbadurlexample.txt ## <Update accordingly>
+SQUIDPATH=/etc/squid ## <UPDATE ACCORDINGLY>
+SQUIDBACKUP=$SQUIDPATH/bckup ## <UPDATE ACCORDINGLY>
+
+SQUIDURLFILE=blacklisted_sites.acl
+
 
 ## preparation - create dir
 mkdir -p $STAGING
@@ -40,3 +45,18 @@ mv $STAGING/list-stg.txt.temp $STAGING/list-stg.txt
 echo -e "
 To test pls check the file :  $STAGING/list-stg.txt
 "
+
+
+## push to squid - not switched on ##
+
+# take bckup
+mkdir -p $SQUIDBACKUP
+cat $SQUIDPATH/$SQUIDURLFILE > $SQUIDBACKUP/$SQUIDURLFILE.bckup.`date +%s`
+
+## transfer new list into squid and remove dedup
+cat $STAGING/list-stg.txt >> $SQUIDPATH/$SQUIDURLFILE
+sort $SQUIDPATH/$SQUIDURLFILE | uniq -u > $SQUIDPATH/$SQUIDURLFILE.tmp
+mv $SQUIDPATH/$SQUIDURLFILE.tmp  $SQUIDPATH/$SQUIDURLFILE
+service squid reload
+
+
