aca2cae8854415dd140c0da4fb098823977f92f8
naimabdrahman <naim.abdrahman@gmail.com>
2019-10-20 20:56:31 +0800

    Create install-weblogic.sh

---
+++ install-weblogic.sh
@@ -0,0 +1,54 @@
+## install Oracle Weblogic Centos 7
+## WIP
+
+
+
+## install java jdk
+## install java - supereasy
+sudo yum -y install java-sdk
+## install java - easy
+#yum -y install java-1.8.0-openjdk
+#yum -y install java-1.8.0-openjdk-devel
+
+
+
+
+## configure path
+cat >> /home/naim/.bash_profile << EOL
+export ORACLE_BASE=/home/naim/wls/oracle 
+export ORACLE_HOME=$ORACLE_BASE/product/fmw12 
+export MW_HOME=$ORACLE_HOME 
+export WLS_HOME=$MW_HOME/wlserver 
+export WL_HOME=$WLS_HOME 
+export DOMAIN_BASE=$ORACLE_BASE/config/domains 
+export DOMAIN_HOME=$DOMAIN_BASE/TEST 
+EOL
+
+mkdir -p $ORACLE_BASE 
+mkdir -p $DOMAIN_BASE 
+mkdir -p $ORACLE_HOME 
+mkdir -p $ORACLE_BASE/config/applications 
+mkdir -p ~/wls/oraInventory 
+
+
+cat > /home/naim/wls/oraInst.loc << EOL
+inventory_loc=/home/naim/wls/oraInventory 
+inst_group=naim
+EOL
+
+cat > /home/naim/wls/wls.rsp << EOL
+[ENGINE] 
+Response File Version=1.0.0.0.0 
+[GENERIC] 
+ORACLE_HOME=/home/naim/wls/oracle/product/fmw12 
+INSTALL_TYPE=WebLogic Server 
+DECLINE_SECURITY_UPDATES=true 
+SECURITY_UPDATES_VIA_MYORACLESUPPORT=false
+EOL
+
+## DL weblogic from Oracle Page
+## username && password is required to dl 
+
+
+## installation /
+java -jar /home/naim/wls/oracle/fmw_12.1.3.0.0_wls.jar -silent -responseFile /home/naim/wls/wls.rsp -invPtrLoc /home/naim/wls/oraInst.loc
