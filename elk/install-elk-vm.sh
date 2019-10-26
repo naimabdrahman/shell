44a332ce3d531c97430d43f5a5a52320e69c44eb
naimabdrahman <naim.abdrahman@gmail.com>
2019-10-26 19:02:15 +0800

    Update install-elk-vm.sh

--- elk/install-elk-vm.sh
+++ elk/install-elk-vm.sh
@@ -4,7 +4,20 @@
 ## install java
 yum -y install java-openjdk-devel java-openjdk
 
-## Add ELK repository
+## Add ELK repository - version 7
+
+#cat <<EOF | sudo tee /etc/yum.repos.d/elasticsearch.repo
+#[elasticsearch-6.x]
+#name=Elasticsearch repository for 6.x packages
+#baseurl=https://artifacts.elastic.co/packages/6.x/yum
+#gpgcheck=1
+#gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+#enabled=1
+#autorefresh=1
+#type=rpm-md
+#EOF
+
+
 cat <<EOF | sudo tee /etc/yum.repos.d/elasticsearch.repo
 [elasticsearch-7.x]
 name=Elasticsearch repository for 7.x packages
