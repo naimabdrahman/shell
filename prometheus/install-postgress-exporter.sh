026517b8bbe4b499b18ac6ccafc803179c592abf
naimabdrahman <naim.abdrahman@gmail.com>
2019-10-31 10:12:33 +0800

    Update install-postgress-exporter.sh

--- prometheus/install-postgress-exporter.sh
+++ prometheus/install-postgress-exporter.sh
@@ -1,1 +1,41 @@
-https://github.com/wrouesnel/postgres_exporter/releases/download/v0.6.0/postgres_exporter_v0.6.0_linux-amd64.tar.gz
+## install postgres exporter centos 7
+## version : node_exporter-0.18
+
+## preparation
+yum -y install wget
+
+
+## download and install
+mkdir /root/dl
+cd /root/dl
+wget "https://github.com/wrouesnel/postgres_exporter/releases/download/v0.6.0/postgres_exporter_v0.6.0_linux-amd64.tar.gz"
+sleep 2
+cd /root
+mkdir /root/postgres_exporter
+cd /root/postgres_exporter
+tar -zxvf /root/dl/postgres_exporter_v0.6.0_linux-amd64.tar.gz 
+sleep 2
+
+
+## create service
+touch /etc/systemd/system/postgres_exporter.service
+cat > /etc/systemd/system/postgres_exporter.service <<EOL
+[Unit]
+Description=Postgres Exporter
+
+[Service]
+User=root
+ExecStart=/root/postgres_exporter/postgres_exporter_v0.6.0_linux-amd64/postgres_exporter 
+
+[Install]
+WantedBy=default.target
+EOL
+
+
+## enable service
+systemctl daemon-reload
+systemctl enable postgres_exporter.service
+systemctl start postgres_exporter.service
+
+
+echo -e "\n\n\nPls browse to http://your_server_ip:postgres_exporter/metrics\n\n\n"
