b41dfe7087adc85b6771eb5748c94779a06b749b
naimabdrahman <naim.abdrahman@gmail.com>
2019-11-04 16:54:47 +0800

    Create install-pgbouncer.sh

---
+++ install-pgbouncer.sh
@@ -0,0 +1,8 @@
+## install pgbouncer centos 7
+
+## repo
+rpm -ivh https://download.postgresql.org/pub/repos/yum/9.2/redhat/rhel-7-x86_64/pgdg-redhat92-9.2-3.noarch.rpm
+
+
+## installation
+yum -y install pbgouncer
