ada03adcc8dbea8041f23def42208b6e58c999e2
naimabdrahman <naim.abdrahman@gmail.com>
2019-11-08 11:57:43 +0800

    Update install-oracledb-exporter.sh

--- prometheus/install-oracledb-exporter.sh
+++ prometheus/install-oracledb-exporter.sh
@@ -1,2 +1,12 @@
 ## Oracle DB exporter
 docker run  --net=host -dit --rm -e DATA_SOURCE_NAME=system/password@localhost iamseth/oracledb_exporter
+
+## verification
+
+echo -e "
+
+To check , pls run below
+curl http://IP:9161/metrics | grep -i oracledb_up
+
+"
+
