0bd690e50ea876b02a37a083e71937334fa81cbd
naimabdrahman <naim.abdrahman@gmail.com>
2019-11-08 15:47:37 +0800

    Update install-oracledb-exporter.sh

--- prometheus/install-oracledb-exporter.sh
+++ prometheus/install-oracledb-exporter.sh
@@ -1,6 +1,9 @@
 ## Oracle DB exporter
 docker run  --net=host -dit --rm -e DATA_SOURCE_NAME=system/password@localhost iamseth/oracledb_exporter
 
+#docker run  --net=host -dit --rm -e DATA_SOURCE_NAME=system/password@localhost:1521 iamseth/oracledb_exporter ## with port number
+
+
 ## verification
 
 echo -e "
