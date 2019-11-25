e6b2aad6bb10b25fe6d2cf9e229c3a5a1b3486c8
naimabdrahman <naim.abdrahman@gmail.com>
2019-11-25 10:22:53 +0800

    Update install-redis-exporter.sh

--- prometheus/install-redis-exporter.sh
+++ prometheus/install-redis-exporter.sh
@@ -1,5 +1,6 @@
 ## run redis exporter from docker image
 ## reference : https://hub.docker.com/r/oliver006/redis_exporter/
 ## reference : https://github.com/oliver006/redis_exporter
+## check port 9121 for monitoring
 
 docker run -dit --name redis_exporter --rm --net=host -e redis.addr=localhost:6379 oliver006/redis_exporter
