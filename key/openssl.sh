8ffd3ba8240e97fc340cef595f6add644429d177
naimabdrahman <naim.abdrahman@gmail.com>
2020-01-29 11:52:34 +0800

    Update openssl.sh

--- key/openssl.sh
+++ key/openssl.sh
@@ -2,10 +2,26 @@
 ## tested in centos 7
 ## referenece ==> https://linuxize.com/post/creating-a-self-signed-ssl-certificate/
 
+
+
+## manual => switched off
+#openssl req -newkey rsa \
+#            -x509 \
+#            -sha256 \
+#            -days 3650 \
+#            -nodes \
+#            -out example.crt \
+#            -keyout example.key
+
+
+
+## manual => switched on
 openssl req -newkey rsa \
             -x509 \
             -sha256 \
             -days 3650 \
             -nodes \
             -out example.crt \
-            -keyout example.key
+            -keyout example.key \
+            -subj "/C=SI/ST=Ljubljana/L=Ljubljana/O=Security/OU=IT Department/CN=www.example.com"
+
