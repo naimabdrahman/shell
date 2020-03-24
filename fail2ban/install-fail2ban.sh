182a57ebc99b766e5641afc3769fc25bfceb9c03
naimabdrahman <naim.abdrahman@gmail.com>
2020-03-25 07:44:03 +0800

    Update install-fail2ban.sh

--- fail2ban/install-fail2ban.sh
+++ fail2ban/install-fail2ban.sh
@@ -7,3 +7,20 @@ yum -y install epel-release
 yum -y install fail2ban
 service fail2ban start
 chkconfig fail2ban on
+
+## fail2ban - customization and service reload - /etc/fail2ban/jail.local
+touch /etc/fail2ban/jail.local
+curl https://raw.githubusercontent.com/naimabdrahman/shell/master/fail2ban/jail.local > /etc/fail2ban/jail.local 
+service fail2ban restart
+
+## verification - notification
+echo -e "
+to validate fail2ban .. run below
+
+fail2ban-client  status
+
+or
+
+fail2ban-client  status sshd
+
+"
