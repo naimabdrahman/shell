924a55ceb7b81fb53e8749bde226c2fc45dffe09
naimabdrahman <naim.abdrahman@gmail.com>
2020-05-22 18:47:07 +0800

    Update install-alertmanager.sh

--- prometheus/install-alertmanager.sh
+++ prometheus/install-alertmanager.sh
@@ -1,7 +1,8 @@
 ## install prometheus alertmanager on centos 7
 
 ## package version to be updated form time to time
-package=alertmanager-0.19.0.linux-amd64
+#package=alertmanager-0.19.0.linux-amd64
+package=alertmanager-0.20.0.linux-amd64
 
 ## prerequisuite
 yum -y install wget
@@ -11,7 +12,8 @@ cd /root/dl
 
 ## dl alertmanager 
 sleep 2
-wget https://github.com/prometheus/alertmanager/releases/download/v0.19.0/$package.tar.gz
+#wget https://github.com/prometheus/alertmanager/releases/download/v0.19.0/$package.tar.gz
+wget https://github.com/prometheus/alertmanager/releases/download/v0.20.0/$package.tar.gz
 sleep 2
 tar -zxvf $package.tar.gz
 mv $package /root/
@@ -29,7 +31,8 @@ Description=AlertManager
 
 [Service]
 User=root
-ExecStart=/root/alertmanager-0.19.0.linux-amd64/alertmanager --config.file /root/alertmanager-0.19.0.linux-amd64/alertmanager.yml
+#ExecStart=/root/alertmanager-0.19.0.linux-amd64/alertmanager --config.file /root/alertmanager-0.19.0.linux-amd64/alertmanager.yml
+ExecStart=/root/alertmanager-0.20.0.linux-amd64/alertmanager --config.file /root/alertmanager-0.20.0.linux-amd64/alertmanager.yml
 
 [Install]
 WantedBy=default.target
