dc3efa43ebbe1dbf02a6fdb63a16c92a1c4b3915
naimabdrahman <naim.abdrahman@gmail.com>
2020-06-20 09:08:06 +0800

    Create install-chrome.sh

---
+++ install-chrome.sh
@@ -0,0 +1,25 @@
+## install chrome/chromium centos7
+## reference : https://geekflare.com/install-chromium-ubuntu-centos/
+
+
+## chrome installation
+cat > /etc/yum.repos.d/google-chrome.repo <<EOL
+[google-chrome]
+name=google-chrome
+baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64
+enabled=1
+gpgcheck=1
+gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub
+EOL
+
+yum install -y google-chrome-stable
+
+
+## chromium installation
+#dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
+yum -y install chromium*
+
+
+## validation rendering testing 
+google-chrome-stable --headless --disable-gpu --no-sandbox --print-to-pdf https://gf.dev/
+
