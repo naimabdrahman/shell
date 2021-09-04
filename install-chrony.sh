68e3569370b12485b9c4a317582981d8c33458c8
naimabdrahman <naim.abdrahman@gmail.com>
2021-09-04 08:54:19 +0800

    Update install-chrony.sh

--- install-chrony.sh
+++ install-chrony.sh
@@ -1,6 +1,13 @@
 ## install chrony on centos 7
 ## fw is off and selinux is off
 
+# install & setup service
 yum -y install chrony 
 service chronyd start
 chkconfig chronyd on
+
+
+# checking services
+#chronyc tracking
+#chronyc sources
+
