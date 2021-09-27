cb699cd9342a57baee6000291c5dba8381303d1f
naimabdrahman <naim.abdrahman@gmail.com>
2021-09-27 11:58:33 +0800

    Create install-google-auth.sh

---
+++ install-google-auth.sh
@@ -0,0 +1,42 @@
+## setup google auth 2fa 
+## tested on centos 8
+
+
+
+## install google-authenticator
+yum -y install google-authenticator
+sleep 5 ; google-authenticator # yyny # manual config
+
+
+
+
+## pam config
+
+cat /etc/pam.d/sshd > /etc/pam.d/sshd.bckup.`date +%s`
+
+cat >> /etc/pam.d/sshd << EOF
+#auth    required      pam_unix.so     no_warn try_first_pass
+auth    required      pam_google_authenticator.so
+EOF
+
+sed -i 's/^auth       substack     password-auth/#auth       substack     password-auth/g' /etc/pam.d/sshd
+
+
+
+
+## sshd config 
+cat /etc/ssh/sshd_config > /etc/ssh/sshd_config.bckup.`date +%s`
+
+sed -i 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/g' /etc/ssh/sshd_config
+ 
+echo "
+# These lines should be added to the end of the file
+Match User example-user
+    AuthenticationMethods keyboard-interactive
+" >> /etc/ssh/sshd_config
+
+service sshd restart
+
+
+
+## ref ==> https://www.linode.com/docs/guides/how-to-use-one-time-passwords-for-two-factor-authentication-with-ssh-on-centos/
