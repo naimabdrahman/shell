681978c9bee9d78a64273814fd3730b843381e96
naimabdrahman <naim.abdrahman@gmail.com>
2022-01-04 09:52:11 +0000

    Tue Jan  4 09:52:11 UTC 2022 nginx custom

--- nginx/install-ngx-custom.sh
+++ nginx/install-ngx-custom.sh
@@ -5,7 +5,7 @@ touch /etc/yum.repos.d/nginx.repo
 cat > /etc/yum.repos.d/nginx.repo <<EOF
 [nginx-stable]
 name=nginx stable repo
-baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
+baseurl=http://nginx.org/packages/centos/\$releasever/\$basearch/
 gpgcheck=1
 enabled=1
 gpgkey=https://nginx.org/keys/nginx_signing.key
@@ -13,14 +13,14 @@ module_hotfixes=true
 
 [nginx-mainline]
 name=nginx mainline repo
-baseurl=http://nginx.org/packages/mainline/centos/$releasever/$basearch/
+baseurl=http://nginx.org/packages/mainline/centos/\$releasever/\$basearch/
 gpgcheck=1
 enabled=0
 gpgkey=https://nginx.org/keys/nginx_signing.key
 module_hotfixes=true
 EOF
 
-yum --disablerepo="*" --enablerepo="nginx-stable" install  nginx
+yum --disablerepo="*" --enablerepo="nginx-stable" install -y nginx
 service nginx start
 chkconfig nginx on
 
