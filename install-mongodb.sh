cd01bd2dca1d4c09251bd71653aaf3393473fee3
naimabdrahman <naim.abdrahman@gmail.com>
2023-04-04 15:28:27 +0800

    Create install-mongodb.sh

---
+++ install-mongodb.sh
@@ -0,0 +1,21 @@
+## install MongoDB Community Edition ##
+
+## repo
+
+cat > /etc/yum.repos.d/mongodb-org-6.0.repo <<EOF
+[mongodb-org-6.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/6.0/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-6.0.asc
+EOF
+
+
+## install
+yum install -y mongodb-org
+
+
+## service
+systemctl start mongod
+chkoconfig mongod on
