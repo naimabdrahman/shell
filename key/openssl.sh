83da429cccac0a98d9558b17a8bb8942892c1abe
naimabdrahman <naim.abdrahman@gmail.com>
2023-04-16 13:25:42 +0800

    Update openssl.sh

--- key/openssl.sh
+++ key/openssl.sh
@@ -25,3 +25,4 @@ openssl req -newkey rsa \
             -keyout example.key \
             -subj "/C=SI/ST=Ljubljana/L=Ljubljana/O=Security/OU=IT Department/CN=www.example.com"
 
+#openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=nginxsvc/O=nginxsvc"
