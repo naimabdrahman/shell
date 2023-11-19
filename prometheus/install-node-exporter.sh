58603381cd51ffab87bef784deb63988fcd844da
naimabdrahman <naim.abdrahman@gmail.com>
2023-11-19 13:36:07 +0800

    Update install-node-exporter.sh

--- prometheus/install-node-exporter.sh
+++ prometheus/install-node-exporter.sh
@@ -1,19 +1,21 @@
-## install node exporter centos 7
-## version : node_exporter-0.18
+## install node exporter centos 8
+## version : node_exporter-1.7
 
 yum -y install wget
 
 mkdir /root/dl
 cd /root/dl
 #wget "https://github.com/prometheus/node_exporter/releases/download/0.11.0/node_exporter-0.11.0.linux-amd64.tar.gz" #old
-wget "https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz"
+wget "https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz" #old
+wget "https://github.com/prometheus/node_exporter/releases/download/v1.7.0/node_exporter-1.7.0.linux-amd64.tar.gz"
 sleep 2
 
 cd /root
 mkdir /root/node_exporter
 cd /root/node_exporter
 #tar -zxvf /root/dl/node_exporter-0.11.0.linux-amd64.tar.gz #old
-tar -zxvf /root/dl/node_exporter-0.18.1.linux-amd64.tar.gz 
+#tar -zxvf /root/dl/node_exporter-0.18.1.linux-amd64.tar.gz #old
+tar -zxvf /root/dl/node_exporter-1.7.0.linux-amd64.tar.gz
 sleep 2
 
 
@@ -24,7 +26,7 @@ echo -e "">> /etc/systemd/system/node_exporter.service
 echo -e "[Service]" >> /etc/systemd/system/node_exporter.service
 echo -e "User=root">> /etc/systemd/system/node_exporter.service
 #echo -e "ExecStart=/root/node_exporter/node_exporter" >> /etc/systemd/system/node_exporter.service #old
-echo -e "ExecStart=/root/node_exporter/node_exporter-0.18.1.linux-amd64/node_exporter" >> /etc/systemd/system/node_exporter.service
+echo -e "ExecStart=/root/node_exporter/node_exporter-1.7.0.linux-amd64/node_exporter" >> /etc/systemd/system/node_exporter.service
 echo -e "" >> /etc/systemd/system/node_exporter.service
 echo -e "[Install]" >> /etc/systemd/system/node_exporter.service
 echo -e "WantedBy=default.target" >> /etc/systemd/system/node_exporter.service
