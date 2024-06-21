734d95140ef50024998c09875ad249788cb95534
naimabdrahman <naim.abdrahman@gmail.com>
2024-06-21 22:19:41 +0800

    Update init-new-vm.sh

--- init-new-vm.sh
+++ init-new-vm.sh
@@ -1,13 +1,13 @@
 ## New VM init
-## tested on centos 7 / centos 8
+## tested on centos 7 / centos 8 / centos 9
 ## password text file is prepared separately prior to running this script
 
 ## epel
 yum -y install epel-release
 
-## firewall 
+## firewall
 service firewalld stop
-chkconfig firewalld off
+systemctl enable firewalld #chkconfig firewalld off
 
 ## selinux off
 setenforce 0
@@ -16,10 +16,10 @@ sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 ## chrony
 yum -y install chrony
 service chronyd start
-chkconfig chronyd on
+systemctl enable chronyd #chkconfig chronyd on
 
 
-## establish ssh keys   
+## establish ssh keys
 cd ~/.ssh ; ssh-keygen -f id_rsa -t rsa -N "" ; sleep 1
 yum -y install sshpass ; sleep 1
 #sshpass -p "`cat /root/password`" ssh-copy-id -o StrictHostKeyChecking=no root@localhost ; sleep 1
@@ -43,23 +43,23 @@ yum -y install vim wget git bind-utils net-tools lsof screen telnet tar tree jq 
 ## fail2ban
 yum -y install fail2ban
 service fail2ban start
-chkconfig fail2ban on
+systemctl enable fail2ban #chkconfig fail2ban on
 
 
 ## PermitRootLogin no  ---> switched off
 PERMITROOT=off       ## on or off <Update accordingly>
-if test "$PERMITROOT" = "on" 
-then 
+if test "$PERMITROOT" = "on"
+then
  cat /etc/ssh/sshd_config > /etc/ssh/sshd_config.bckup.`date +%s`
  sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
-else 
+else
   echo -e "PERMITROOT is not on"
 fi
- 
+
 
 ## reload sshd
 systemctl reload sshd
- 
+
 
 ## remove unwanted files and dir
 rm -rf /root/git
@@ -71,4 +71,4 @@ rm -rf /root/password
 
 
 ## hostname - for cluster mode
-#/etc/hosts - for cluster mode 
+#/etc/hosts - for cluster mode
